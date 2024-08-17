b4622ed4762eeb1d84e294e078e59decd08d5315
code-qtzl <azepeda90@outlook.com>
2024-08-17 16:42:18 -0500

    getURLsFromString function added

--- crawl.js
+++ crawl.js
@@ -1,3 +1,31 @@
+const { JSDOM } = require('jsdom');
+
+function getUrlFromSting(htmlBody, baseURL) {
+	const urls = [];
+	const dom = new JSDOM(htmlBody);
+	const linkElements = dom.window.document.querySelectorAll('a');
+	for (const linkElement of linkElements) {
+		if (linkElement.href.slice(0, 1) === '/') {
+			// relative
+			try {
+				const urlObj = new URL(`${baseURL}${linkElement.href}`);
+				urls.push(urlObj.href);
+			} catch (err) {
+				console.log(`Error with relative URL: ${err.message}`);
+			}
+		} else {
+			// absolute
+			try {
+				const urlObj = new URL(linkElement.href);
+				urls.push(urlObj.href);
+			} catch (err) {
+				console.log(`Error with relative URL: ${err.message}`);
+			}
+		}
+	}
+	return urls;
+}
+
 function normalizeURL(url) {
 	// return url;
 	const urlObj = new URL(url);
@@ -12,4 +40,5 @@ function normalizeURL(url) {
 
 module.exports = {
 	normalizeURL,
+	getUrlFromSting,
 };
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normalizeURL } = require('./crawl.js'); //stripProtocol
+const { normalizeURL, getUrlFromSting } = require('./crawl.js');
 const { test, expect } = require('@jest/globals');
 
 test('normalizeURL, and strips protocol', () => {
@@ -29,6 +29,55 @@ test('normalizeURL, strip http', () => {
 	const input = 'http://www.Google.com/';
 	const actual = normalizeURL(input);
 	const expected = 'www.google.com';
+
+	expect(actual).toEqual(expected);
+});
+
+test('getUrlFromHTML, absolute', () => {
+	const inputHTMLBody = `
+    <html><body><a href="https://developer.mozilla.org/">URL: hostname</a></body></html>
+    `;
+	const inputBaseURL = 'https://developer.mozilla.org/';
+	const actual = getUrlFromSting(inputHTMLBody, inputBaseURL);
+	const expected = ['https://developer.mozilla.org/'];
+
+	expect(actual).toEqual(expected);
+});
+
+test('getUrlFromHTML, relative', () => {
+	const inputHTMLBody = `
+    <html><body><a href="/path/">URL: hostname</a></body></html>
+    `;
+	const inputBaseURL = 'https://developer.mozilla.org';
+	const actual = getUrlFromSting(inputHTMLBody, inputBaseURL);
+	const expected = ['https://developer.mozilla.org/path/'];
+
+	expect(actual).toEqual(expected);
+});
+
+test('getUrlFromHTML, for both absolute and relative', () => {
+	const inputHTMLBody = `
+    <html><body>
+		<a href="https://developer.mozilla.org/path1/">URL: hostname One</a>
+		<a href="/path2/">URL: hostname Two</a>
+	</body></html>
+    `;
+	const inputBaseURL = 'https://developer.mozilla.org';
+	const actual = getUrlFromSting(inputHTMLBody, inputBaseURL);
+	const expected = [
+		'https://developer.mozilla.org/path1/',
+		'https://developer.mozilla.org/path2/',
+	];
+	expect(actual).toEqual(expected);
+});
+
+test('getUrlFromHTML, invalid', () => {
+	const inputHTMLBody = `
+    <html><body><a href="invalid">Invalid URL</a></body></html>
+    `;
+	const inputBaseURL = 'https://developer.mozilla.org';
+	const actual = getUrlFromSting(inputHTMLBody, inputBaseURL);
+	const expected = [];
 
 	expect(actual).toEqual(expected);
 });
